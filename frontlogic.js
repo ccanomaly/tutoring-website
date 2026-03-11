520725e36de8935f1f72852e09f72851cc94bcb1
ccanomaly <cayala15@wgu.edu>
2026-03-10 19:27:06 -0700

    Add about me page and contact form

---
+++ frontlogic.js
@@ -0,0 +1,11 @@
+let button = document.querySelector(".info-button")
+let logoButton = document.querySelector(".title")
+
+logoButton.addEventListener("click", () =>{
+    window.location.href= "./index.html"
+
+})
+
+button.addEventListener("click", () => {
+    window.location.href= "./about.html"
+});
